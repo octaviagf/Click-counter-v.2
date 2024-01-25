5bf3a989aef84725a046f47e185a9243ed63f654
octaviagf <oagf0616@gmail.com>
2024-01-24 21:15:44 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <ClickCounter />
+    </div>
+  );
+}
+
+function ClickCounter() {
+  const [count, setCount] = useState(0);
+
+  function handlePrev() {
+    if (count > 0) setCount(count - 1);
+  }
+
+  function handleNext() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <div className="container">
+        <button className="btn" onClick={handlePrev}>
+          -
+        </button>
+        <p>{count}</p>
+        <button className="btn" onClick={handleNext}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
